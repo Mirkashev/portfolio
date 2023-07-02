88f12a634c008861073ee962f306ac2e58fc7a9c
Ilnaz <mirkashev007@gmail.com>
2023-07-02 15:01:55 +0300

    remove nda

--- doodle-logi/gulp/config/plugins.js
+++
@@ -1,9 +0,0 @@
-import browserSync from "browser-sync";
-// import plumber from "gulp-plumber";
-// import notify from "gulp-notify";
-
-export const plugins = {
-  browserSync: browserSync,
-  // plumber: plumber,
-  // notify: notify,
-};
--- flappy-chicken/public/src/js/globals/index.js
+++
@@ -1,2 +0,0 @@
-const API_URL = "https://flappy.black-sheep.ru";
-export default API_URL;
